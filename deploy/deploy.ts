2e1bcedfaf8eb19b508dbac057ebcacbdf45d007
Plopmenz <plopmenz@gmail.com>
2024-01-18 20:41:58 +0100

    deployment settings added

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -2,12 +2,17 @@ import { Address, Deployer } from "../web3webdeploy/types";
 import { deployCounter } from "./counters/Counter";
 import { deployProxyCounter } from "./counters/ProxyCounter";
 
+export interface DeploymentSettings {}
+
 export interface Deployment {
   counter: Address;
   proxyCounter: Address;
 }
 
-export async function deploy(deployer: Deployer): Promise<Deployment> {
+export async function deploy(
+  deployer: Deployer,
+  settings?: DeploymentSettings
+): Promise<Deployment> {
   const counter = await deployCounter(deployer);
   const proxyCounter = await deployProxyCounter(deployer, counter);
   return {
